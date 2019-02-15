dcb9876b92c01a8ff653b73e648321d26f54fb39
ZXeRRinor <antpotapov51@gmail.com>
2019-02-15 19:42:37 +0300

    1: Getting arguments and open input and output files

---
+++ LW1/lw1_1.rb
@@ -0,0 +1,7 @@
+input_file_name, output_file_name, search_string, replace_string = ARGV
+
+File.open(input_file_name, 'r') do |in_file|
+  File.open(output_file_name, 'w+') do |out_file|
+
+  end
+end
