99c806fe96be30507b75430463a8764e79787a13
ZXeRRinor <antpotapov51@gmail.com>
2019-02-15 19:50:10 +0300

    1: Done

--- LW1/lw1_1.rb
+++ LW1/lw1_1.rb
@@ -2,6 +2,8 @@ input_file_name, output_file_name, search_string, replace_string = ARGV
 
 File.open(input_file_name, 'r') do |in_file|
   File.open(output_file_name, 'w+') do |out_file|
-
+    while str = in_file.gets
+      out_file.puts(str.chomp.gsub(Regexp.new(search_string), replace_string))
+    end
   end
 end
