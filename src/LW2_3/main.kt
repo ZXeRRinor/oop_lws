11db5bb4284e0140c62364972faa65303642b0c4
ZXeRRinor <antpotapov51@gmail.com>
2019-03-09 14:09:47 +0300

    3: Add newWords map for implement option of selecting save or not save new words after words

--- src/LW2_3/main.kt
+++ src/LW2_3/main.kt
@@ -1,5 +1,33 @@
 package LW2_3
 
+import java.io.File
+
 fun main(args: Array<String>) {
+    val words: MutableMap<String, String> = mutableMapOf()
+    val newWords: MutableMap<String, String> = mutableMapOf()
+    File("src/LW2_3/dictionary").forEachLine {
+        val line = it.split('=')
+        words[line[0]] = line[1]
+    }
+    var input: String? = readLine()
+    while (input != "...") {
+        if (input != null) {
+            var translation: String? = words[input]
+            if (translation != null) {
+                translation = newWords[input]
+            }
+            if (translation != null) {
+                println("$input => $translation")
+            } else {
+                println("Неизвестное слово $input. Введите перевод или пустую строку для отказа.")
+                translation = readLine()
+                if (translation != null && translation != "") {
+                    File("src/LW2_3/dictionary").appendText("\n$input=$translation")
+                    newWords[input] = translation
+                }
+            }
+        }
+        input = readLine()
+    }
 
 }
