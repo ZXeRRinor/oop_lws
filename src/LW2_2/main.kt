1f36fc4353632392a4a654ca2dac6aab715800c8
ZXeRRinor <antpotapov51@gmail.com>
2019-03-16 09:15:00 +0300

    2: Tests done, launch procedure 2: Done

--- src/LW2_2/main.kt
+++ src/LW2_2/main.kt
@@ -1,8 +1,12 @@
 package LW2_2
 
-fun main(args: Array<String>) {
+fun launch() {
     val input: String? = readLine()
     if (input != null) {
         println(htmlDecode(input))
     }
+}
+
+fun main(args: Array<String>) {
+    launch()
 }
--- src/LW2_2/test/HTMLDecodeKtTest.kt
+++ src/LW2_2/test/HTMLDecodeKtTest.kt
@@ -1,5 +1,6 @@
 package LW2_2.test
 
+import LW2_2.htmlDecode
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
@@ -7,6 +8,37 @@ import org.junit.jupiter.api.Assertions.*
 internal class HTMLDecodeKtTest {
 
     @Test
-    fun htmlDecode() {
+    fun htmlDecode_blankLine_test() {
+        assertEquals("", htmlDecode(""))
+    }
+
+    @Test
+    fun htmlDecode_onlySymbols_test() {
+        assertEquals("&<>", htmlDecode("&amp;&lt;&gt;"))
+    }
+
+    @Test
+    fun htmlDecode_oneSymbol_test() {
+        assertEquals("&", htmlDecode("&amp;"))
+    }
+
+    @Test
+    fun htmlDecode_noSymbols_test() {
+        assertEquals("hello world", htmlDecode("hello world"))
+    }
+
+    @Test
+    fun htmlDecode_symbolAtStartOfString_test() {
+        assertEquals("&hello world", htmlDecode("&amp;hello world"))
+    }
+
+    @Test
+    fun htmlDecode_symbolAtEndOfString_test() {
+        assertEquals("hello world&", htmlDecode("hello world&amp;"))
+    }
+
+    @Test
+    fun htmlDecode_someSymbolsAtEnd_test() {
+        assertEquals("hello world&&&&", htmlDecode("hello world&amp;&amp;&amp;&amp;"))
     }
 }
