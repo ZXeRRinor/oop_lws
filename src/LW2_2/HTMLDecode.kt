7d275fcfbfbd8abe1daa4c1973b39dcb6853fd2f
ZXeRRinor <antpotapov51@gmail.com>
2019-03-16 12:06:47 +0300

    2: Algorithm changed 2: Done

--- src/LW2_2/HTMLDecode.kt
+++ src/LW2_2/HTMLDecode.kt
@@ -1,52 +1,16 @@
 package LW2_2
 
 fun htmlDecode(in_str: String): String {
-    var rangeStart = -1
-    var rangeEnd: Int
     var result = in_str
-    var replacement:String
-    var i = 0
-    while (i < result.length) {
-        if (i >= result.length) {
-            break
-        }
-        if (result[i] == '&') {
-            rangeStart = i
-        }
-        if (result[i] == ';') {
-            rangeEnd = i
-            if ((rangeStart != -1) and (rangeEnd - rangeStart > 1)) {
-                var lengthOfReplacement = 0
-                when (result.substring(rangeStart + 1, rangeEnd)) {
-                    "quot" -> {
-                        replacement = "\""
-                        lengthOfReplacement = 2 + 4 - 1
-                    }
-                    "apos" -> {
-                        replacement = "\'"
-                        lengthOfReplacement = 2 + 4 - 1
-                    }
-                    "lt" -> {
-                        replacement = "<"
-                        lengthOfReplacement = 2 + 2 - 1
-                    }
-                    "gt" -> {
-                        replacement = ">"
-                        lengthOfReplacement = 2 + 2 - 1
-                    }
-                    "amp" -> {
-                        replacement = "&"
-                        lengthOfReplacement = 2 + 3 - 1
-                    }
-                    else -> replacement = ""
-                }
-                result = result.replaceRange(rangeStart, rangeEnd + 1, replacement)
-                i -= lengthOfReplacement
-
-            }
-            rangeStart = -1
-        }
-        i++
+    val replacements: Map<String, Char> = mapOf(
+        "apos" to '\'',
+        "amp" to '&',
+        "lt" to '<',
+        "gt" to '>',
+        "quot" to '\"'
+    )
+    for (i in replacements.keys) {
+        result = result.replace("&$i;", replacements[i]!!.toString())
     }
     return result
 }
