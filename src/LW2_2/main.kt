ac7d8c837ccb89b0d0cffff18f0ba7078b6fb65d
ZXeRRinor <antpotapov51@gmail.com>
2019-03-16 09:16:24 +0300

    2: Done

--- src/LW2_2/main.kt
+++ src/LW2_2/main.kt
@@ -1,10 +1,8 @@
 package LW2_2
 
 fun launch() {
-    val input: String? = readLine()
-    if (input != null) {
-        println(htmlDecode(input))
-    }
+    val input: String = readLine() ?: ""
+    println(htmlDecode(input))
 }
 
 fun main(args: Array<String>) {
