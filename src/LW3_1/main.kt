aeb8094f7c7e295c833b313ce6b4282dbe5b346e
ZXeRRinor <antpotapov51@gmail.com>
2019-04-13 00:35:42 +0300

    1: CLI for class Car

--- src/LW3_1/main.kt
+++ src/LW3_1/main.kt
@@ -1,9 +1,39 @@
 package LW3_1
 
 fun main() {
-    var input_line = readLine() ?: ""
-    while(input_line != "quit")
-    {
-
+    var inputLine: String = readLine() ?: ""
+    val car = Car()
+    while (inputLine != "quit") {
+        try {
+            when (inputLine) {
+                //"Info" -> car.info
+                "EngineOn" -> car.turnOnEngine()
+                "EngineOff" -> car.turnOffEngine()
+                else -> {
+                    val command = inputLine.split(' ')[0]
+                    val value = inputLine.split(' ')[1].toInt()
+                    when (command) {
+                        "SetGear" -> {
+                            car.setGear(
+                                when (value) {
+                                    -1 -> Car.Gear.REVERSE
+                                    0 -> Car.Gear.NEUTRAL
+                                    1 -> Car.Gear.FIRST
+                                    2 -> Car.Gear.SECOND
+                                    3 -> Car.Gear.THIRD
+                                    4 -> Car.Gear.FOURTH
+                                    5 -> Car.Gear.FIFTH
+                                    else -> Car.Gear.NEUTRAL
+                                }
+                            )
+                        }
+                        "SetSpeed" -> car.setSpeed(value)
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            println(e.message)
+        }
+        inputLine = readLine() ?: ""
     }
 }
