040a49a21ac103fefa8400ae0727586792f6a75d
ZXeRRinor <antpotapov51@gmail.com>
2019-04-16 20:59:35 +0300

    1: Canvas class transformed to Canvas interface.

--- src/LW4_1/graphics/Shapes/Canvas.kt
+++ src/LW4_1/graphics/Shapes/Canvas.kt
@@ -1,5 +1,23 @@
 package LW4_1.graphics.Shapes
 
-class Canvas(val width: Int, val height: Int, val title: String) {
+interface Canvas {
+    val width: Int
+    val height: Int
+    val title: String
 
+    fun drawLine() {
+
+    }
+
+    fun drawCircle() {
+
+    }
+
+    fun drawRectangle() {
+
+    }
+
+    fun drawTriangle() {
+
+    }
 }
