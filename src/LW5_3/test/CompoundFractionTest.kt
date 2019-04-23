78dba5c97e1bdfec2fe296f08d06226b31da779b
ZXeRRinor <antpotapov51@gmail.com>
2019-04-24 00:23:21 +0300

    3: CompoundFraction equals() tests

--- src/LW5_3/test/CompoundFractionTest.kt
+++ src/LW5_3/test/CompoundFractionTest.kt
@@ -4,6 +4,7 @@ import LW5_3.CompoundFraction
 import LW5_3.Rational
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import kotlin.test.assert
 
 internal class CompoundFractionTest {
     @Test
@@ -26,5 +27,16 @@ internal class CompoundFractionTest {
         val rat1 = Rational(17, 14)
         val compFract = CompoundFraction(1, 3, 14)
         assert(compFract.equals(rat1))
+    }
+
+    @Test
+    internal fun equalsTest() {
+        val rat1 = Rational(17, 14)
+        val compFract1 = CompoundFraction(1, 3, 14)
+        val compFract2 = CompoundFraction(2, 14, 14)
+        val compFract3 = CompoundFraction(3)
+        assert(compFract1.equals(rat1))
+        assert(compFract2.equals(3))
+        assert(compFract2 == compFract3)
     }
 }
